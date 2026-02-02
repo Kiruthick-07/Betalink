485f5e13111223f40caa9f5d636a98ae523f0d17
Kiruthick R <152460421+Kiruthick-07@users.noreply.github.com>
2026-02-02 14:56:50 +0530

    Auth error resolved

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -32,9 +32,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
 
     try {
@@ -42,10 +42,9 @@ userSchema.pre('save', async function (next) {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         console.log('Password hashed successfully');
-        next();
     } catch (error) {
         console.error('Password hashing error:', error);
-        next(error);
+        throw error;
     }
 });
 
